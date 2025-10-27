e11c5f78d35e5cf64bf90f3f96bb5e5e5f5a66b6
tetehoma <tetehoma@gmail.com>
2025-10-27 09:50:06 +0900

    acrescentando narracao de voz

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ exibirMensagemInicial();
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate: 1.2});
 }
 function exibirMensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
